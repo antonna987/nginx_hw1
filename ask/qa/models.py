f0c7e5037f17449c98a019e9241f266b2c0a37b8
Anton Nazin <anton.na987@gmail.com>
2020-03-09 23:13:54 +0300

    fix failed test case 5

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
+
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
@@ -21,9 +28,3 @@ class Answer(models.Model):
     added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
-
-class QuestionManager(models.Manager):
-    def new(self):
-        return self.order_by('-added_at')
-    def popular(self):
-        return self.order_by('-rating')
